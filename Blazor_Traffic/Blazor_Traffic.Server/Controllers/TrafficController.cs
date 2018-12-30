3d1aee9df1639ac29b8a0c75fe51495d40da34f7
Zlatko Rusev <zlatko.rusev@students.softuni.bg>
2018-12-30 23:34:04 +0200

    OnGround schedule

--- Blazor_Traffic/Blazor_Traffic.Server/Controllers/TrafficController.cs
+++ Blazor_Traffic/Blazor_Traffic.Server/Controllers/TrafficController.cs
@@ -1,11 +1,12 @@
 ﻿namespace Blazor_Traffic.Server.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
     using Services;
     using Shared;
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
+    using System.IO;
+    using System.Text;
     using System.Threading.Tasks;
 
     [Route("api/[controller]")]
@@ -18,23 +19,6 @@
             _trafficService = trafficService;
         }
 
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
-        }
-
         [HttpGet("[action]")]
         public async Task<IEnumerable<Station>> All()
         {
@@ -45,6 +29,23 @@
         public async Task<StationTimes> ById(int id)
         {
             return await _trafficService.GetStationById($"/v1/metro/times/{id}");
+        }
+
+        [HttpGet("[action]")]
+        public JsonResult GetStopsList()
+        {
+            string text;
+
+            var fileStream = new FileStream("./Data/coordinates_with_linetypes.json", FileMode.Open, FileAccess.Read);
+
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                text = streamReader.ReadToEnd();
+            }
+
+            List<Stop> items = JsonConvert.DeserializeObject<List<Stop>>(text);
+
+            return Json(items);
         }
     }
 }
---
+++ Blazor_Traffic/Blazor_Traffic.Shared/Stop.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor_Traffic.Shared
+{
+    public class Stop
+    {
+        public int StopCode { get; set; }
+
+        public int[] LineTypes { get; set; }
+
+        public string StopName { get; set; }
+
+        public decimal[] Coordinates { get; set; }
+    }
+}
